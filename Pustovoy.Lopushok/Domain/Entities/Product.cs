9ad1ee1237c99e6fcbc89ca1ff66ad692a8bab6e
hulkiedo19 <mob.newinhulkizmishenko@gmail.com>
2022-10-27 16:52:14 +0300

    home #2

--- Pustovoy.Lopushok/Domain/Entities/Product.cs
+++ Pustovoy.Lopushok/Domain/Entities/Product.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Pustovoy.Lopushok.Infrastucture.Persistence;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Pustovoy.Lopushok.Domain.Entities
 {
     public partial class Product
     {
         private string? _image;
+        private decimal _minCostForAgent;
 
         public Product()
         {
@@ -28,7 +33,54 @@ namespace Pustovoy.Lopushok.Domain.Entities
         }
         public int? ProductionPersonCount { get; set; }
         public int? ProductionWorkshopNumber { get; set; }
-        public decimal MinCostForAgent { get; set; }
+        public decimal MinCostForAgent 
+        {
+            get => _minCostForAgent;
+            set => _minCostForAgent = value;
+        }
+
+        public string? Materials
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                List<ProductMaterial> productMaterials = new ApplicationDbContext().ProductMaterials
+                    .Include(m => m.Material)
+                    .Where(p => p.ProductId == this.Id)
+                    .ToList();
+
+                if (productMaterials.Count == 0)
+                    return "Материалов нет";
+
+                sb.Append("Материалы: ");
+
+                foreach (ProductMaterial pm in productMaterials)
+                    sb.Append($"{pm.Material.Title}, ");
+
+                sb.Remove(sb.Length - 2, 2);
+
+                return sb.ToString();
+            }
+        }
+        public decimal Cost
+        {
+            get
+            {
+                List<ProductMaterial> productMaterials = new ApplicationDbContext().ProductMaterials
+                    .Include(m => m.Material)
+                    .Where(p => p.ProductId == this.Id)
+                    .ToList();
+
+                if (productMaterials.Count == 0)
+                    return _minCostForAgent;
+
+                decimal cost = 0;
+                foreach (ProductMaterial pm in productMaterials)
+                    cost += pm.Material.Cost;
+
+                return cost;
+            }
+        }
 
         public virtual ProductType? ProductType { get; set; }
         public virtual ICollection<ProductCostHistory> ProductCostHistories { get; set; }
